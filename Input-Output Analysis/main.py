06f956f6378a93638c2bbe815ed8acc69d98f486
Mohammad Amin Tahavori <50955527+mohammadamint@users.noreply.github.com>
2022-04-11 09:57:17 +0200

    Impact Assessment
    - Adding the fucntins to calculate the impacts and indicators

--- Input-Output Analysis/main.py
+++ Input-Output Analysis/main.py
@@ -1,6 +1,27 @@
 #%%
 import sys
+import pandas as pd
 
+def calc_PRoI(investment_scenario,saving_scenario):
+
+    investment = investment_scenario['V'].sum().sum()
+    saving = saving_scenario['V'].sum().sum()
+
+    return saving/investment
+
+
+def calc_PPBT(PRoI):
+    return 1/PRoI
+
+
+def calc_impact(investment_scenario,saving_scenario,p_lifetime,impact,matrix):
+
+    saving = saving_scenario[matrix].loc[impact,:].sum().sum()
+    investment = investment_scenario[matrix].loc[impact,:].sum().sum()
+
+    return investment - p_lifetime * saving
+
+#%%
 sys.path.append("/Users/mohammadamintahavori/Documents/GitHub/MARIO")
 
 from mario import parse_from_excel, set_log_verbosity
@@ -27,7 +48,13 @@ print(kenya)
 # Checking the database balance
 kenya.is_balanced("coefficients")
 #%%
-scenarios = ["Biomass", "Ecopulpers", "Shading Trees"]
+# Scenarios and their p_lifetime
+
+scenarios = {
+    "Biomass" : 10, 
+    "Ecopulpers" : 10, 
+    "Shading Trees" : 10
+    }
 
 #%%
 for scenario in scenarios:
@@ -36,13 +63,67 @@ for scenario in scenarios:
     # Investment Phase
     kenya.shock_calc(io=io, Y=True, scenario=f"{scenario} Investment")
     kenya.shock_calc(
-        io=io, z=True, v=True, e=True, scenario=f"{scenario} non Investment"
+        io=io, z=True, v=True, e=True, scenario=f"{scenario} Saving"
     )
 #%%
 # aggregating the database for better visulization
-kenya_aggregated = kenya.aggregate(
+kenya.aggregate(
     io="Data/aggregation.xlsx",
     levels=["Activity", "Commodity", "Factor of production"],
-    inplace=False,
 )
+
+#%%
+impacts_satellite = {
+    "Water" : ['Blue Water','Grey Water','Green Water'],
+    "Land"  : ["Proxy Land","Arable land","Permanent crops",],
+    "CO2"   : ["CO2 by Electricity and Heat","CO2 by Industry","CO2 by Transport","CO2 by Residential","CO2 by Cement production","CO2 by Other",]
+}
+
+impact_value_added = {
+    "Labor"   : ["Labor - Skilled","Labor - Semi Skilled","Labor - Unskilled",],
+    "Capital" : ["Capital - Machines"],
+    "Imports" : ["Import"]
+}
+
+
+
+#%%
+columns = ['PRoI',"PPBT"] + [*impacts_satellite] + [*impact_value_added]
+results = pd.DataFrame(0,index=scenarios,columns=columns)
+
+# %%
+# calculating the PRoI
+
+for scenario,p_lifetime in scenarios.items():
+
+    investment_scenario = kenya[f"{scenario} Investment"]
+    saving_scenario = kenya[f"{scenario} Saving"]
+
+    PRoI = calc_PRoI(
+        investment_scenario= investment_scenario,
+        saving_scenario= saving_scenario,
+        )
+
+    results.loc[scenario,"PRoI"] = PRoI
+    results.loc[scenario,"PPBT"] = calc_PPBT(PRoI)
+
+    for impact,items in impacts_satellite.items():
+        results.loc[scenario,impact] = calc_impact(
+            investment_scenario=investment_scenario,
+            saving_scenario=saving_scenario,
+            p_lifetime=p_lifetime,
+            impact = items,
+            matrix = "E"
+        )
+
+    for impact,items in impact_value_added.items():
+        results.loc[scenario,impact] = calc_impact(
+            investment_scenario=investment_scenario,
+            saving_scenario=saving_scenario,
+            p_lifetime=p_lifetime,
+            impact = items,
+            matrix = "V"
+        )
+
+
 # %%
